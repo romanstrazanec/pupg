ec2d2e6788e7a73ea274ee2c66ad507c4da0ce2e
romanstrazanec <roman.strazanec007@gmail.com>
2019-08-14 11:12:01 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='romanstrazanec_pip_upgrade_all',
+      version='1.0.0',
+      description='Upgrade all python packages.',
+      author='Roman Stražanec',
+      author_email='roman.strazanec007@gmail.com',
+      license='MIT',
+      url='https://github.com/romanstrazanec/pip_upgrade_all',
+      install_requires=['pip']
+      )
