cb9789f33dce402a90a53eb4fe17bfc7c516230e
romanstrazanec <roman.strazanec007@gmail.com>
2019-09-28 13:25:26 +0200

    add dist

--- setup.py
+++ setup.py
@@ -10,5 +10,14 @@ setup(name='romanstrazanec_pip_upgrade_all',
       author_email='roman.strazanec007@gmail.com',
       license='MIT',
       url='https://github.com/romanstrazanec/pip_upgrade_all',
-      install_requires=['pip']
+      download_url='https://github.com/romanstrazanec/pip_upgrade_all/releases/tag/v1.1.0',
+      install_requires=['pip'],
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Environment :: Console",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 3.7",
+          "Topic :: Other/Nonlisted Topic"
+      ]
       )
