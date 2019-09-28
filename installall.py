e5f51565465ecce1eaf6c7579ea226d448c7bdb0
romanstrazanec <roman.strazanec007@gmail.com>
2019-09-28 11:58:11 +0200

    use concurrent.futures.thread

--- installall.py
+++ installall.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 from argparse import ArgumentParser, Namespace as ArgNamespace
 from subprocess import run as subproc, CompletedProcess, CalledProcessError
+from concurrent.futures.thread import ThreadPoolExecutor
 from typing import List
 
 
 def get_args() -> ArgNamespace:
     parser = ArgumentParser(description='Upgrade all your python packages.')
-    parser.add_argument('-l', '--limit', type=int, default=7,
-                        help='set default upgrade try limit (default=7)')
-    parser.add_argument('-mp', '--multiproc', action='store_true',
-                        help='''use multiprocessing library to distribute
-                                upgrades across multiple processes''')
+    parser.add_argument('-l', '--limit', type=int, default=3,
+                        help='set default upgrade try limit (default=3)')
     parser.add_argument('-c', '--check', action='store_true',
                         help='check if upgrade was successful')
     parser.add_argument('-v', '--verbose', action='store_true')
@@ -23,7 +21,7 @@ def pip(*args) -> List[str]:
 
 def get_outdated_pckgs() -> List[str]:
     res = subproc(pip('list', '-o'), capture_output=True, text=True)
-    return [line.split(' ')[0] for line in res.stdout.split('\n')[1:]]
+    return [line.split(' ')[0] for line in res.stdout.split('\n')[2:]]
 
 
 def upgrade(pckg: str, dutl: int, ntry: int = 0) -> bool:
@@ -43,15 +41,9 @@ if __name__ == "__main__":
     if len(pckgs) == 0:
         exit('All packages are up to date!')
 
-    if args.multiproc:
-        from multiprocessing import Process as Proc
-        procs = (Proc(target=upgrade, args=(pckg, args.limit)) for pckg in pckgs)
-
-        for p in procs:
-            p.start()
-    else:
+    with ThreadPoolExecutor() as executor:
         for pckg in pckgs:
-            upgrade(pckg, args.limit)
+            executor.submit(upgrade, pckg, args.limit)
 
     if args.check:
         print('Checking outdated packages...')
